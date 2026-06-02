2b6f208a55d40b57e1784833fc75eeb9a1279438
Peter Janicka <peterj@amarulasolutions.com>
2026-06-02 15:22:47 +0200

    Centralize jvm version in .toml catalog
    The set of jvmTarget and Java source/target compatibility changed to
    use value from the .toml catalog.
    
    Signed-off-by: Peter Janicka <peterj@amarulasolutions.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.toVersion(libs.versions.jvm.get())
+        targetCompatibility = JavaVersion.toVersion(libs.versions.jvm.get())
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = libs.versions.jvm.get()
     }
     buildFeatures {
         compose = true
--- kmp-money/build.gradle.kts
+++ kmp-money/build.gradle.kts
@@ -26,11 +26,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.toVersion(libs.versions.jvm.get())
+        targetCompatibility = JavaVersion.toVersion(libs.versions.jvm.get())
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = libs.versions.jvm.get()
     }
 }
 
