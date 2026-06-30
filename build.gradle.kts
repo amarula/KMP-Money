22c687555724b4eb5312f030dc7cdd4db54ebe22
Peter Janicka <peterj@amarulasolutions.com>
2026-06-30 11:26:03 +0200

    Add Kover plugin
    Added Kover for generating test coverage.
    Updated verification pipeline script to generate and record coverage.
    
    Change-Id: Ia54669ed3b9e1fddf4e7e5685bc1c9328b4228b7
    Signed-off-by: Peter Janicka <peterj@amarulasolutions.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.kover) apply false
 }
 
 tasks.register<Copy>("installGitHook") {
--- kmp-money/build.gradle.kts
+++ kmp-money/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.kotlin.multiplatform.library)
     alias(libs.plugins.ktlint)
+    alias(libs.plugins.kover)
 }
 
 kotlin {
