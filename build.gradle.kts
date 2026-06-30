50eb9406b40e5d53356957e7ef82f7cee0e8a163
Peter Janicka <peterj@amarulasolutions.com>
2026-06-30 09:51:23 +0200

    Add Ktlint to project
    Added ktlint plugin for code format checking and autoformatting.
    Added pre-push hook script that ensures that pushed code is according to
    the code style.
    Added .editorconfig with rules for ktlint.
    
    Change-Id: I6efbc68faf67491b9f973f4e6bb08fcf464ce683
    Signed-off-by: Peter Janicka <peterj@amarulasolutions.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,29 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
+    alias(libs.plugins.ktlint) apply false
+}
+
+tasks.register<Copy>("installGitHook") {
+    val sourceHook = rootProject.layout.projectDirectory.file("scripts/pre-push")
+    val targetDir = rootProject.layout.projectDirectory.dir("../.git/hooks")
+
+    from(sourceHook)
+    into(targetDir)
+
+    doLast {
+        val hookFile = targetDir.file("pre-push").asFile
+        if (hookFile.exists()) {
+            hookFile.setExecutable(true, false)
+            println("✅ Git hook installed successfully at: ${hookFile.path}")
+        } else {
+            println("❌ Failed to find installed hook at: ${hookFile.path}")
+        }
+    }
+}
+
+gradle.projectsEvaluated {
+    tasks.matching { it.name == "prepareKotlinBuildScriptModel" }.configureEach {
+        dependsOn("installGitHook")
+    }
 }
