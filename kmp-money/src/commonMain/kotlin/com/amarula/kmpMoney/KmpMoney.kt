c1f0dcbdf09d75bb7b41d4af9ad3f77ceae34b96
Ronnie Otieno <ronnie.otieno@amarulasolutions.com>
2026-09-22 08:54:20 +0300

    Add isLessThanOrEqualTo
    Inclusive counterpart to isLessThan, completing the four
    inclusive/strict comparison pairs JSR-354's MonetaryAmount defines.
    Enforces currency consistency like the other cross-amount comparisons.
    
    Change-Id: I50530b977d8809a405c2edfdac522a73cdeb56c2

--- kmp-money/src/commonMain/kotlin/com/amarula/kmpMoney/KmpMoney.kt
+++ kmp-money/src/commonMain/kotlin/com/amarula/kmpMoney/KmpMoney.kt
@@ -351,6 +351,16 @@ data class KmpMoney(private val amount: BigDecimal, val currency: Currency) : Co
         return this.amount >= other.amount
     }
 
+    /**
+     * Returns `true` if this amount is less than or equal to [other].
+     *
+     * @throws IllegalArgumentException if [other] has a different currency.
+     */
+    fun isLessThanOrEqualTo(other: KmpMoney): Boolean {
+        requireSameCurrency(other)
+        return this.amount <= other.amount
+    }
+
     /** Returns `true` if [other] has the same currency as this amount. */
     fun isSameCurrency(other: KmpMoney): Boolean = this.currency == other.currency
 
